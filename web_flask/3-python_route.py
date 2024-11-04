12b27b59c2678452d0fd321d335e85aae1a61651
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-04 13:08:23 -0600

    copied 2 into 3, added Python message, _ turned into spaces, default value of text = is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    print Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    print HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
+def python_text(text):
+    """
+    display “Python ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
